fe6526b1f53b3db82aa594354b200b92208edb2f
srmtwenty <68181312+srmtwenty@users.noreply.github.com>
2021-03-25 21:43:50 -0700

    added statics folder

---
+++ EasyShop-master/ecommerce_app/static/project_script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $("#login").hover(function () {
+        $("#log-form").fadeIn(400);
+    });
+
+    $("#log-form").mouseleave(function () {
+        $("#log-form").fadeOut(400);
+    }); 
+    
+});
