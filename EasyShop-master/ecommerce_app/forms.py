f0c2f4d4339426de22ab53bbc523bc6f58badefa
srmtwenty <68181312+srmtwenty@users.noreply.github.com>
2021-03-25 21:41:15 -0700

    Added EasyShop Master

---
+++ EasyShop-master/ecommerce_app/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import *
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'price', 'description','img']
